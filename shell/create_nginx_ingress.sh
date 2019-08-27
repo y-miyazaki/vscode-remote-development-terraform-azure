be1805a414f43bfba54aa7e385a863cc7ca6e763
y-miyazaki <ymiyazakixyz@gmail.com>
2019-08-27 23:41:24 +0900

    fixed shell.

--- shell/create_nginx_ingress.sh
+++ shell/create_nginx_ingress.sh
@@ -12,11 +12,10 @@ if [ -z $IP ]; then
 fi
 
 # Create a namespace for your ingress resources
-kubectl create namespace ingress-basic
+# kubectl create namespace ingress-basic
 
 # Use Helm to deploy an NGINX ingress controller
 helm install stable/nginx-ingress \
-    --namespace ingress-basic \
     --set controller.replicaCount=2 \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
